869dccabafa25f52a4f49d818cab5108e1d38854
HyeLee <39701029+hlpark0209@users.noreply.github.com>
2021-12-16 02:11:28 +0900

    Add JS scroll to section function ( include contact section )

--- main.js
+++ main.js
@@ -10,3 +10,29 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar-white');
     }
 });
+
+// Make scroll to section
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+    
+    // link가 없는 경우는 클릭이 제한됨 
+    const target = e.target;
+    const link = target.dataset.link;
+    if ( link == null ){
+        return;
+    } 
+    scrollIntoView(link);
+    
+});
+
+// Move to contact section
+const contact = document.querySelector('.home__contact');
+contact.addEventListener('click', () => {
+    scrollIntoView('#contact');
+});
+
+// Add function
+function scrollIntoView(selector){
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
